9a6fc2aa74672e9561575e556c8106be13d8dc9b
Jaime Tejeiro <Jaimetejeiro@gmail.com>
2024-10-02 16:03:20 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jaime Tejeiro on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

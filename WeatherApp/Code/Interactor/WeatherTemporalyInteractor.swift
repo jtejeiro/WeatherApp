dbe8f5d833fb6c551125eb14ec64ecb1723cffec
Jaime Tejeiro <Jaimetejeiro@gmail.com>
2024-10-09 11:45:08 +0200

    [WTemporaly] Implement persistent temporary weather list [SearchCity]  - Ux change

---
+++ WeatherApp/Code/Interactor/WeatherTemporalyInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherTemporalyInteractor.swift
+//  WeatherApp
+//
+//  Created by Jaime Tejeiro on 7/10/24.
+//
+
+import Foundation
---
+++ WeatherApp/Code/ModelLogic/WeatherTemporalyLogic.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherTemporalyLogic.swift
+//  WeatherApp
+//
+//  Created by Jaime Tejeiro on 7/10/24.
+//
+
+import Foundation
